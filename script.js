f9f9ef1024ba3871216ba10da41dc7b505426169
Lumo357 <90748318+Lumo357@users.noreply.github.com>
2021-09-15 22:20:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,30 @@ $(function() {
               $('.player-detail').fadeOut();
           });  
       }
+       
+       $('.player-top-list').slick({
+        infinite: true,
+        slidesToShow: 3,
+        //slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ] 
+
+    });
     
 
 });
