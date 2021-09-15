4f5d3e28d2aeaf603139b4d23031185069bdd1a9
Lumo357 <90748318+Lumo357@users.noreply.github.com>
2021-09-15 15:36:02 +0900

    Add files via upload

---
+++ about/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABOUT</title>
+    <link rel="stylesheet" href="../style.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    
+</head>
+<body>
+    <header>
+        <div id="top-logo">
+            <a href="../index.php">
+                <img src="../images/logo.jfif" alt="MvLのロゴ">
+            </a>
+        </div>
+
+        <div>
+            <ul id="top-menu">
+                <li><a href="index.php">ABOUT</a></li>
+                <li><a href="../players/index.php">PLAYERS</a></li>
+                <li><a href="../news/index.php">NEWS</a> </li>
+                
+            </ul>
+        </div>
+
+        <div id="menu-bar">
+
+        </div>
+
+    </header>
+
+    <main>
+        <div class="about-wrapper">
+            <h1>ABOUT US</h1>
+            <div class="about-us">
+            <h2>BRクラン【Mí Vida Łoca】</h2>
+            <h3>クランマスター: 
+                <a href="https://twitter.com/Mv59688079" target="_blank">
+                MVL≫la岡田お彡
+                <i class="fab fa-twitter-square twitter-icon"></i>
+                </a>
+            </h3> 
+            <ul>
+                <li><a href="#">Players</a> : 18名</li>
+                <li>Founded : 2021.2.24</li>
+                <li>Achievement : 優勝経験 複数回</li>
+                <li>Activity-Time : 21時〜25時</li>
+                <li>To-Join-Us : 
+                    <ul>
+                        <li>名前に MvŁ» を付けれる方</li>
+                        <li>VC可能な方（聞き専△）</li>
+                        <li>プラベ意欲のある方 入隊試験有り</li>
+                    </ul>
+                    『募集枠』 メンバー1名 MG1名 気になった方は
+                    <a href="https://twitter.com/Mv59688079" target="_blank">    
+                    DMまで
+                    </a>
+                </li>
+
+            </ul>
+            </div>    
+        </div>
+        
+    </main>
+
+    <footer>
+        <div class="bottom-menu">
+            <h2>CONTACT</h2>
+            <a href="https://twitter.com/Mv59688079" target="_blank">
+            <i class="fab fa-twitter-square twitter-icon"></i>
+            https://twitter.com/Mv59688079
+            </a>
+        </div>
+
+        <small>&copy;Mí Vida Łoca</small>
+    </footer>
+</body>
+</html>
